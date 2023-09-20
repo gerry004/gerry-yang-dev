3f44fecc8ec73259bf777217061c3bfef0cca3e5
Gerry Yang <gerry04y@gmail.com>
2023-09-20 14:50:29 +0100

    Desktop Navbar and Contact

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     <div className="flex flex-col justify-center items-center text-center py-8 px-2 md:mt-16">
       <img src={headshot} alt="Gerry Yang" className="rounded-full border border-gray shadow-gray shadow-md object-cover h-72 w-72 m-6" />
       <h1 className="text-primary">Gerry Yang</h1>
-      <p className="py-4">I'm an MSISS student at Trinity College Dublin, Former Co-Founder of Kreoh, Naughton Scholar, Patch 2021 Alumni, but really - I'm a self taught programmer that loves figuring out new ideas and building cool stuff.</p>
+      <p className="p-4">I'm an MSISS student at Trinity College Dublin, Former Co-Founder of Kreoh, Naughton Scholar, Patch 2021 Alumni, but really - I'm a self taught programmer that loves figuring out new ideas and building cool stuff.</p>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,9 @@ function Navbar(props) {
   }
 
   return (
-    <nav ref={navbar} id="navbar" className="hidden md:flex flex-row justify-center h-16 px-6 py-2 fixed top-0 right-0 left-0 bg-white z-50">
-      <button onClick={() => goTo(navbar)} className="hidden md:block ml-2 text-xl font-bold">Gerry Yang</button>
-      <div className="flex flex-row w-full justify-evenly md:w-1/3 md:ml-auto">
+    <nav ref={navbar} id="navbar" className="hidden md:flex flex-row justify-center h-16 px-6 py-2 fixed top-0 right-0 left-0 bg-primary z-50">
+      <button onClick={() => goTo(navbar)} className="hidden md:block ml-2 text-2xl font-semibold text-white">Gerry Yang</button>
+      <div className="flex flex-row w-full justify-evenly md:w-1/3 ml-auto text-white">
         <button onClick={() => goTo(projectsRef)}>Projects</button>
         <button onClick={() => goTo(experienceRef)}>Experience</button>
         <button onClick={() => goTo(contactRef)}>Contact</button>
