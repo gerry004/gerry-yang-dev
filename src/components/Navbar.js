7d4846eaac40b552bdac3116e10228a67d1c9797
Gerry Yang <gerry04y@gmail.com>
2023-09-20 14:57:31 +0100

    Navbar Resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,10 +13,11 @@ function Navbar(props) {
 
   return (
     <nav ref={navbar} id="navbar" className="hidden md:flex flex-row justify-center h-16 px-6 py-2 fixed top-0 right-0 left-0 bg-primary z-50">
-      <button onClick={() => goTo(navbar)} className="hidden md:block ml-2 text-2xl font-semibold text-white">Gerry Yang</button>
-      <div className="flex flex-row w-full justify-evenly md:w-1/3 ml-auto text-white">
+      <button onClick={() => goTo(navbar)} className="hidden md:block m-2 text-2xl font-semibold text-white">Gerry Yang</button>
+      <div className="flex flex-row w-full justify-evenly items-center md:w-1/2 ml-auto text-white">
         <button onClick={() => goTo(projectsRef)}>Projects</button>
         <button onClick={() => goTo(experienceRef)}>Experience</button>
+        <a href="https://drive.google.com/file/d/1Bg2wTxCRDBKf9GwrqIqedWG6u5NlgURa/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
         <button onClick={() => goTo(contactRef)}>Contact</button>
       </div>
     </nav>
